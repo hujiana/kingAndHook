51a11b0b140a531fec781048676673785b02f80a
king <923789900@qq.com>
2019-12-12 14:27:38 +0800

    Initial commit

---
+++ app/src/main/java/com/instagram/android/Manager.java
@@ -0,0 +1,26 @@
+package com.instagram.android;
+
+import android.app.Application;
+
+import cn.andHook.xposed.XposedHelpers;
+
+
+/**
+ * instagram Hooks
+ */
+public class Manager {
+
+
+    /**
+     * 初始化hook方法加载
+     * @param application 程序的application
+     */
+    public static void init(Application application)
+    {
+
+
+
+    }
+
+
+}
